bfeae70ccead37f03a1c11d08d223cfcfd52fded
Anton O <mavarazy@gmail.com>
2015-03-30 14:33:09 +0300

    removing templates

--- common-web/src/main/java/com/clemble/casino/server/spring/common/JsonSpringConfiguration.java
+++ common-web/src/main/java/com/clemble/casino/server/spring/common/JsonSpringConfiguration.java
@@ -1,5 +1,15 @@
 package com.clemble.casino.server.spring.common;
 
+import com.clemble.casino.bet.json.BetJsonModule;
+import com.clemble.casino.game.json.GameJsonModule;
+import com.clemble.casino.goal.json.GoalJsonModule;
+import com.clemble.casino.json.CommonJsonModule;
+import com.clemble.casino.json.GenericJsonModule;
+import com.clemble.casino.payment.json.PaymentJsonModule;
+import com.clemble.casino.player.json.PlayerJsonModule;
+import com.clemble.casino.player.json.PresenceJsonModule;
+import com.clemble.casino.tag.json.TagJsonModule;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +22,56 @@ public class JsonSpringConfiguration implements SpringConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return ObjectMapperUtils.OBJECT_MAPPER;
+    }
+
+    @Bean
+    public JodaModule jodaModule() {
+        return new JodaModule();
+    }
+
+    @Bean
+    public GenericJsonModule genericJsonModule() {
+        return new GenericJsonModule();
+    }
+
+    @Bean
+    public CommonJsonModule commonJsonModule() {
+        return new CommonJsonModule();
+    }
+
+    @Bean
+    public PlayerJsonModule playerJsonModule() {
+        return new PlayerJsonModule();
+    }
+
+    @Bean
+    public PresenceJsonModule presenceJsonModule() {
+        return new PresenceJsonModule();
+    }
+
+    @Bean
+    public GameJsonModule gameJsonModule() {
+        return new GameJsonModule();
+    }
+
+    @Bean
+    public BetJsonModule betJsonModule() {
+        return new BetJsonModule();
+    }
+
+    @Bean
+    public GoalJsonModule goalJsonModule() {
+        return new GoalJsonModule();
+    }
+
+    @Bean
+    public PaymentJsonModule paymentJsonModule() {
+        return new PaymentJsonModule();
+    }
+
+    @Bean
+    public TagJsonModule tagJsonModule() {
+        return new TagJsonModule();
     }
 
 }
