1c645fc28cff4dd658b141598c77377a429bdf14
Anton O <mavarazy@gmail.com>
2015-04-13 20:35:09 +0300

    Updating CORSFilter

--- common-web/src/main/java/com/clemble/casino/server/spring/WebBootSpringConfiguration.java
+++ common-web/src/main/java/com/clemble/casino/server/spring/WebBootSpringConfiguration.java
@@ -1,17 +1,27 @@
 package com.clemble.casino.server.spring;
 
 import com.clemble.casino.server.spring.common.SpringConfiguration;
+import com.google.common.collect.ImmutableMap;
+import com.thetransactioncompany.cors.CORSConfiguration;
+import com.thetransactioncompany.cors.CORSConfigurationException;
 import com.thetransactioncompany.cors.CORSFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.WebApplicationInitializer;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by mavarazy on 3/30/15.
@@ -25,28 +35,24 @@ import java.io.IOException;
 public class WebBootSpringConfiguration implements SpringConfiguration {
 
     @Bean
-    public CORSFilter corsFilter() {
-        CORSFilter corsFilter = new CORSFilter();
-        return corsFilter;
+    public CORSFilterInitializer filterInitializer(){
+        return new CORSFilterInitializer();
     }
 
-    public static class SimpleCORSFilter implements Filter {
-
-        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-            HttpServletResponse response = (HttpServletResponse) res;
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Credentials", "false");
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-Type, playerId, sessionId, tableId");
-            chain.doFilter(req, res);
-        }
-
-        public void init(FilterConfig filterConfig) {
-        }
-
-        public void destroy() {
-        }
+    public static class CORSFilterInitializer implements ServletContextInitializer {
+        @Override
+        final public void onStartup(ServletContext container) throws ServletException {
+           // Step 1. Creating CORS filter
+           FilterRegistration.Dynamic filter = container.addFilter("CORS", CORSFilter.class);
+           if (filter != null) {
+               filter.setInitParameters(ImmutableMap.of(
+                       "cors.allowOrigin", "*",
+                       "cors.supportedHeaders", "Accept, Origin, Content-Type, playerId, sessionId, tableId",
+                       "cors.supportedMethods", "GET, POST, HEAD, OPTIONS, DELETE"
+               ));
+               filter.addMappingForUrlPatterns(null, false, "/*");
+           }
+       }
     }
 
 }
