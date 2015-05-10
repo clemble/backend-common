0f5b5d556fc57fd2ceb86733a042da93bed17831
Anton O <mavarazy@gmail.com>
2015-05-10 10:34:28 +0300

    Adding accomplishments

--- common-web/src/main/java/com/clemble/casino/server/ServerController.java
+++ common-web/src/main/java/com/clemble/casino/server/ServerController.java
@@ -5,5 +5,5 @@ import com.clemble.casino.ClembleService;
 /**
  * Created by mavarazy on 22/12/13.
  */
-public interface ExternalController extends ClembleService {
+public interface ServerController extends ClembleService {
 }
